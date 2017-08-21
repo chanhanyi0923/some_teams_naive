547e7a9241df2b56dfe15724bc45adb5ebdcfece
chanhanyi0923 <chanhanyi0923@gmail.com>
2017-08-21 18:50:04 +0800

    Update F.cpp

--- 0821/F.cpp
+++ 0821/F.cpp
@@ -1,119 +1,75 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 #define fi first
 #define se second
-#define mp(a, b) make_pair(a, b)
 
 typedef long long ll;
 typedef pair<int, int> pii;
-typedef pair<pii, int> ppi;
+typedef pair<pii, pii> ppp;
 
-class BIT
+#define SIZE 100005
+
+vector<int> G[SIZE];
+ppp info[SIZE];
+int pos1[SIZE];
+int pos2[SIZE];
+ll bit[2 * SIZE];
+ll ans[SIZE];
+
+void add(int x, ll y, int N)
 {
-public:
-	int SIZE;
-	static const int BIAS = 5;
-	vector<ll> u;
-	void clear(int N)
-	{
-		SIZE = N;
-		u.resize(SIZE);
-		for (int i = 0; i < SIZE; i ++) u[i] = 0;
-	}
-	void add(int x, ll v)
-	{
-		for (x += BIAS; x < SIZE; x += x & -x) u[x] += v;
-	}
-	ll sum(int x)
-	{
-		ll s = 0;
-		for (x += BIAS; x; x -= x & -x) s += u[x];
-		return s;
-	}
-};
-
-class TreeDiv
+    for (int i = x; i <= N; i += i & (-i)) {
+        bit[i] += y;
+    }
+}
+ll sum(int x)
 {
-public:
-	static const int SIZE = 100005;
-	int sz[SIZE], lv[SIZE], rt[SIZE], fa[SIZE];
-	int cnt, seg[SIZE], idx[SIZE], low[SIZE], top[SIZE], len[SIZE];
-	int init(int n, const vector<int> e[]) {
-		memset(len, cnt = 0, sizeof(len));
-		memset(lv, 0, sizeof(lv));
-		for (int i = 0; i < n; i ++) if (!lv[i]) {
-			segment(rt[i] = fa[i] = i, e);
-			top[seg[i]] = fa[i] = SIZE - 1;
-		}
-		return cnt;
-	}
-private:
-	void segment(int x, const vector<int> e[])
-	{
-		int y, t = -1;
-		sz[x] = 1;
-		rt[x] = rt[fa[x]];
-		lv[x] = lv[fa[x]] + 1;
-		for (size_t i = 0; i < e[x].size(); i ++) if (e[x][i] != fa[x]) {
-			fa[y = e[x][i]] = x;
-			segment(y, e);
-			sz[x] += sz[y];
-			if (t < 0 || sz[y] > sz[t]) t = y;
-		}
-		seg[x] = ~t ? seg[t] : cnt;
-		idx[x] = ~t ? idx[t] + 1 : 0;
-		if (t < 0) low[cnt ++] = x;
-		len[seg[x]] ++;
-		top[seg[x]] = fa[x];
-	}
-};
+    ll s = 0;
+    for (int i = x; i > 0; i -= i & (-i)) {
+        s += bit[i];
+    }
+    return s;
+}
 
-BIT B[100005];
-vector<int> fat[100005];//father
-int node_idx[100005];
-TreeDiv TD;
-ppi node[100005];
+void dfs(int v, int &o)
+{
+    pos1[v] = o ++;
+    for (int i = 0; i < G[v].size(); i ++) {
+        dfs(G[v][i], o);
+    }
+    pos2[v] = o ++;
+}
 
 int main()
 {
-	int N;
-	scanf("%d", &N);
-	for (int i = 0; i < N; i ++) {
-		int p, r, t;
-		scanf("%d%d%d", &p, &r, &t);
-		p --;
-		if (p >= 0) fat[i].push_back(p);
-		node[i] = ppi(pii(r, t), i);
-	}
-	int cnt = TD.init(N, fat);
-	for (int i = 0; i < cnt; i ++) {
-		int j, k = 0;
-		for (j = TD.low[i]; j != TD.top[i]; j = TD.fa[j]) {
-			node_idx[j] = k ++;
-		}
-		B[i].clear(k);
-	}
-	//
-	
-	sort(node, node + N);
-	for (int i = 0; i < N; i ++) {
-		int v = node[i].se;
-		int chain = TD.seg[v];
-		// printf("%d\n", chain);
-		printf("%lld\n", B[chain].sum(node_idx[v]));
-		B[chain].add(node_idx[v], node[i].fi.se);
-		int j = chain;
-		while (true) {
-
-			int ff = TD.top[j];
-			// printf("%d\n", ff);
-			// printf("%d\n", ff);
-			if (ff == TD.SIZE - 1) break;
-			j = TD.seg[ff];
-			B[j].add(0, node[i].fi.se);
-		}
-	}
-	return 0;
+    int N, root = -1;
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i ++) {
+        int p, r, t;
+        scanf("%d%d%d", &p, &r, &t);
+        info[i - 1] = ppp(pii(r, 0), pii(i, t));
+        if (p == -1) {
+            root = i;
+        } else {
+            G[p].push_back(i);
+        }
+    }
+    int _o = 0; dfs(root, _o);
+    for (int i = 0; i < N; i ++) {
+        info[i].fi.se = pos1[info[i].se.fi];
+    }
+    sort(info, info + N);
+    for (int i = 0; i < N; i ++) {
+        int idx = info[i].se.fi;
+        ans[idx] = sum(pos2[idx]) - sum(pos1[idx] - 1);
+        add(pos2[idx], info[i].se.se, 2 * N);
+    }
+    for (int i = 1; i <= N; i ++) {
+        printf("%lld\n", ans[i]);
+    }
+    return 0;
 }
